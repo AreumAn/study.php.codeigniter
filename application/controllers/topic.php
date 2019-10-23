44f4607994a2e0c3642fb803fc7a623425f20ed8
Areum An <areum@venngage.com>
2019-10-22 22:22:46 -0400

    Add view

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -4,31 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Topic extends CI_Controller {
     // http://localhost:8080/index.php/topic
     function index(){
-        echo '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
-                Topic main page
-            </body>
-        </html>
-        ';
+        $this->load->view('topic');
     }
 
     // http://localhost:8080/index.php/topic/get/$id
     function get($id){
-        echo '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
-                Topic '.$id.'
-            </body>
-        </html>
-        ';
+        $data = array('id'=>$id);
+        $this->load->view('get', $data);
     }
 }
